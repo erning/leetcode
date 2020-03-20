c4f22760eb0d6bac953fb9343e6e686dceb83963
Zhang Erning <zhang@erning.com>
2020-03-20 17:16:06 +0800

    4 - Median of Two Sorted Arrays

--- 0004-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
+++ 0004-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -1,5 +1,79 @@
 package solution
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	return 0
+	len1, len2 := len(nums1), len(nums2)
+	if len1 > len2 {
+		// ensure len(nums1) <= len(nums2)
+		len1, len2 = len2, len1
+		nums1, nums2 = nums2, nums1
+	}
+	// fmt.Printf("%v, %v\n", nums1, nums2)
+
+	if len2 == 0 {
+		// both arrays are empty?
+		return 0
+	}
+	if len2 == 1 {
+		// nums2 has only one item
+		if len1 == 0 {
+			return float64(nums2[0])
+		}
+		return float64(nums1[0]+nums2[0]) / 2
+	}
+
+	a, b, c, d := 0, len1, (len1+1)/2, 0
+	for {
+		d = (len1+len2+1)/2 - c
+		// fmt.Printf("a=%d b=%d c=%d d=%d\n", a, b, c, d)
+
+		if c > 0 && d < len2 && nums1[c-1] > nums2[d] {
+			b = c
+			c = a + (b-a)/2
+			continue
+		}
+		if c < len1 && d > 0 && nums2[d-1] > nums1[c] {
+			a = c
+			c = a + (b-a+1)/2
+			continue
+		}
+		break
+	}
+
+	max := func(x, y int) int {
+		if x > y {
+			return x
+		}
+		return y
+	}
+	min := func(x, y int) int {
+		if x < y {
+			return x
+		}
+		return y
+	}
+
+	x, y := func() (int, int) {
+		if len1 == 0 {
+			return nums2[d-1], nums2[d]
+		}
+		if c == 0 {
+			if d == len2 {
+				return nums2[d-1], nums1[c]
+			}
+			return nums2[d-1], min(nums1[c], nums2[d])
+		}
+		if c == len1 {
+			if d == 0 {
+				return nums1[c-1], nums2[d]
+			}
+			return max(nums1[c-1], nums2[d-1]), nums2[d]
+		}
+		return max(nums1[c-1], nums2[d-1]), min(nums1[c], nums2[d])
+	}()
+
+	if (len1+len2)%2 == 1 {
+		// odd
+		return float64(x)
+	}
+	return float64(x+y) / 2
 }
--- 0004-median-of-two-sorted-arrays/median-of-two-sorted-arrays_test.go
+++ 0004-median-of-two-sorted-arrays/median-of-two-sorted-arrays_test.go
@@ -8,15 +8,27 @@ func TestFindMedianSortedArrays(t *testing.T) {
 
 		t.Logf("expected: %v, output: %v", expected, output)
 		if output != expected {
-			t.Error()
+			t.FailNow()
 		}
 	}
-
-	tf([]int{1, 2, 3}, []int{1, 2, 3}, 2.0)
 
 	tf([]int{1, 3}, []int{2}, 2.0)
 	tf([]int{1, 2}, []int{3, 4}, 2.5)
 
+	tf([]int{3, 4}, []int{1, 2}, 2.5)
+	tf([]int{1}, []int{2, 3}, 2.0)
+
 	tf([]int{1, 2}, []int{3}, 2.0)
 	tf([]int{1, 3}, []int{2, 4}, 2.5)
+	tf([]int{1, 4}, []int{2, 3}, 2.5)
+
+	tf([]int{1, 1, 1, 4}, []int{1, 2, 3, 4}, 1.5)
+	tf([]int{1, 2, 3, 4}, []int{1, 1, 1, 4}, 1.5)
+
+	tf([]int{1, 1, 1, 4}, []int{}, 1)
+
+	tf([]int{}, []int{}, 0)
+	tf([]int{}, []int{1}, 1)
+	tf([]int{}, []int{1, 2}, 1.5)
+	tf([]int{1}, []int{2}, 1.5)
 }
