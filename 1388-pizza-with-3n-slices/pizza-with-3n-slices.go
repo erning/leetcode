593611933813c91179103bf82d7074a13b978f36
Zhang Erning <zhang@erning.com>
2020-03-27 17:00:17 +0800

    optimized memory

--- 1388-pizza-with-3n-slices/pizza-with-3n-slices.go
+++ 1388-pizza-with-3n-slices/pizza-with-3n-slices.go
@@ -12,12 +12,14 @@ func maxSizeSlices(slices []int) int {
 		return b
 	}
 
-	m0 := make([][]int, slen-1) // pick first number
-	m1 := make([][]int, slen-1) // do not pick the first number
-
-	for i := 0; i < 2 && i < slen-1; i++ {
+	m0 := make([][]int, 4) // pick first number
+	m1 := make([][]int, 4) // do not pick the first number
+	for i := 0; i < 4; i++ {
 		m0[i] = make([]int, C+1)
 		m1[i] = make([]int, C+1)
+	}
+
+	for i := 0; i < 2 && i < slen-1; i++ {
 		for w := 1; w < C+1; w++ {
 			m0[i][w] = slices[i]
 			m1[i][w] = slices[i+1]
@@ -25,26 +27,16 @@ func maxSizeSlices(slices []int) int {
 	}
 
 	for i := 2; i < slen-1; i++ {
-		m0[i] = make([]int, C+1)
-		m1[i] = make([]int, C+1)
 		for w := 1; w < C+1; w++ {
 			if i >= 3 {
-				m0[i-2][w] = max(m0[i-3][w], m0[i-2][w])
-				m1[i-2][w] = max(m1[i-3][w], m1[i-2][w])
+				m0[(i-2)%4][w] = max(m0[(i-3)%4][w], m0[(i-2)%4][w])
+				m1[(i-2)%4][w] = max(m1[(i-3)%4][w], m1[(i-2)%4][w])
 			}
-			m0[i][w] = max(m0[i-2][w], slices[i]+m0[i-2][w-1])
-			m1[i][w] = max(m1[i-2][w], slices[i+1]+m1[i-2][w-1])
+			m0[i%4][w] = max(m0[(i-2)%4][w], slices[i]+m0[(i-2)%4][w-1])
+			m1[i%4][w] = max(m1[(i-2)%4][w], slices[i+1]+m1[(i-2)%4][w-1])
 		}
 	}
 
-	// for i := slen - 2; i >= 0; i-- {
-	// 	fmt.Printf("%v - %v: %v\n", i, slices[i], m0[i])
-	// }
-	// for i := slen - 2; i >= 0; i-- {
-	// 	fmt.Printf("%v - %v: %v\n", i+1, slices[i+1], m1[i])
-	// }
-	// fmt.Printf("\n")
-
-	a1, a2, b1, b2 := m0[slen-3][C], m0[slen-2][C], m1[slen-3][C], m1[slen-2][C]
+	a1, a2, b1, b2 := m0[(slen-3)%4][C], m0[(slen-2)%4][C], m1[(slen-3)%4][C], m1[(slen-2)%4][C]
 	return max(max(max(a1, a2), b1), b2)
 }
