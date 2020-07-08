f2c4e43bf3a82862ea000243257fa70a81a95395
Zhang Erning <zhang@erning.com>
2020-07-09 02:09:13 +0800

    burst-force which would be timed out

--- 1499-max-value-of-equation/max-value-of-equation.go
+++ 1499-max-value-of-equation/max-value-of-equation.go
@@ -1,5 +1,20 @@
 package solution
 
 func findMaxValueOfEquation(points [][]int, k int) int {
-	return 0
+	length := len(points)
+	maxV := -1 << 63
+	for i := 0; i < length-1; i++ {
+		for j := i + 1; j < length; j++ {
+			x1, y1 := points[i][0], points[i][1]
+			x2, y2 := points[j][0], points[j][1]
+			if x2-x1 > k {
+				continue
+			}
+			v := y1 + y2 + x2 - x1
+			if v > maxV {
+				maxV = v
+			}
+		}
+	}
+	return maxV
 }
--- 1499-max-value-of-equation/max-value-of-equation_test.go
+++ 1499-max-value-of-equation/max-value-of-equation_test.go
@@ -13,6 +13,13 @@ func TestMaxValueOfEquation(t *testing.T) {
 		}
 	}
 
-	tf([][]int{{1, 3}, {2, 0}, {5, 10}, {6, -10}}, 10, 4)
+	tf([][]int{{1, 3}, {2, 0}, {5, 10}, {6, -10}}, 1, 4)
 	tf([][]int{{0, 0}, {3, 0}, {9, 2}}, 3, 3)
+
+	// {
+	// 	file, _ := ioutil.ReadFile("case-63.json")
+	// 	data := [][]int{}
+	// 	_ = json.Unmarshal([]byte(file), &data)
+	// 	tf(data, 200000000, 0)
+	// }
 }
