75ae2e401297d72df44ebd1e1c41ca02dc61b9e4
Zhang Erning <zhang@erning.com>
2020-07-10 16:43:12 +0800

    Monotonic queue

--- 1499-max-value-of-equation/max-value-of-equation.go
+++ 1499-max-value-of-equation/max-value-of-equation.go
@@ -1,6 +1,37 @@
 package solution
 
 func findMaxValueOfEquation(points [][]int, k int) int {
+	// Monotonic Queue
+	n := len(points)
+	rv := -1 << 63
+	monoque := make([][2]int, n)
+	mqh := 0 // queue head
+	mql := 0 // queue length
+	for i := 0; i < n; i++ {
+		x, y := points[i][0], points[i][1]
+		// pop left while xi-xj > k
+		for mql > 0 && monoque[mqh][1] < x-k {
+			mqh++
+			mql--
+		}
+		if mql > 0 {
+			v := monoque[mqh][0] + y + x
+			if v > rv {
+				rv = v
+			}
+		}
+		// pop right keep max
+		for mql > 0 && monoque[mqh+mql-1][0] <= y-x {
+			mql--
+		}
+		mql++
+		monoque[mqh+mql-1][0] = y - x
+		monoque[mqh+mql-1][1] = x
+	}
+	return rv
+}
+
+func findMaxValueOfEquationSimple(points [][]int, k int) int {
 	length := len(points)
 	maxV := -1 << 63
 	for i := 0; i < length-1; i++ {
--- 1499-max-value-of-equation/max-value-of-equation_test.go
+++ 1499-max-value-of-equation/max-value-of-equation_test.go
@@ -1,6 +1,8 @@
 package solution
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"testing"
 )
 
@@ -16,10 +18,10 @@ func TestMaxValueOfEquation(t *testing.T) {
 	tf([][]int{{1, 3}, {2, 0}, {5, 10}, {6, -10}}, 1, 4)
 	tf([][]int{{0, 0}, {3, 0}, {9, 2}}, 3, 3)
 
-	// {
-	// 	file, _ := ioutil.ReadFile("case-63.json")
-	// 	data := [][]int{}
-	// 	_ = json.Unmarshal([]byte(file), &data)
-	// 	tf(data, 200000000, 0)
-	// }
+	{
+		file, _ := ioutil.ReadFile("case-63.json")
+		data := [][]int{}
+		_ = json.Unmarshal([]byte(file), &data)
+		tf(data, 200000000, 399134490)
+	}
 }
