7f68c27e180d64114931afb28e98b7ed7ea21516
Zhang Erning <zhang@erning.com>
2020-07-23 16:28:19 +0800

    23 - Merge with Divide And Conquer

--- 0023-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 0023-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -15,6 +15,42 @@ type ListNode struct {
 }
 
 func mergeKLists(lists []*ListNode) *ListNode {
+	n := len(lists)
+	if n <= 0 {
+		return nil
+	}
+	if n <= 1 {
+		return lists[0]
+	}
+	for interval := 1; interval < n; interval = interval << 1 {
+		for i := 0; i < n-interval; i = i + interval<<1 {
+			lists[i] = merge2List(lists[i], lists[i+interval])
+		}
+	}
+	return lists[0]
+}
+
+func merge2List(list1, list2 *ListNode) *ListNode {
+	head := &ListNode{}
+	node := head
+	for ; list1 != nil && list2 != nil; node = node.Next {
+		if list1.Val < list2.Val {
+			node.Next = list1
+			list1 = list1.Next
+		} else {
+			node.Next = list2
+			list2 = list2.Next
+		}
+	}
+	if list1 == nil {
+		node.Next = list2
+	} else {
+		node.Next = list1
+	}
+	return head.Next
+}
+
+func mergeKListsTrick(lists []*ListNode) *ListNode {
 	var p []int
 	for _, list := range lists {
 		for node := list; node != nil; node = node.Next {
