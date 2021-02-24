ab50fefebae9856b00a3c7f0c1faa6eb4503bb9a
Zhang Erning <zhang@erning.com>
2021-02-24 15:44:40 +0800

    rename test case

--- 0679-24-game/24GameTests.swift
+++ 0679-24-game/24GameTests.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import LeetCode
 
 extension LeetCodeTests {
-    func test24Game() {
+    func testJudgePoint24() {
         XCTAssertTrue(solution.judgePoint24([4, 1, 8, 7]))
         XCTAssertTrue(solution.judgePoint24([1, 5, 5, 5]))
         XCTAssertTrue(solution.judgePoint24([7, 7, 3, 3]))
--- LeetCodeTests.swift
+++ LeetCodeTests.swift
@@ -3,18 +3,11 @@ import XCTest
 @testable import LeetCode
 
 class LeetCodeTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertTrue(true)
-    }
-
     static var allTests = [
-        ("testExample", testExample),
-        ("testTwoSum", testTwoSum),
-        ("testSolveNQueens", testSolveNQueens),
-        ("test24Game", test24Game),
+        ("0001-testTwoSum", testTwoSum),
+        ("0009-testIsPalindrome", testIsPalindrome),
+        ("0037-testSolveNQueens", testSolveNQueens),
+        ("0679-testJudgePoint24", testJudgePoint24),
     ]
 }
 
