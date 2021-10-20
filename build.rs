41824f87363aecdbd6560a52ed6f5eb27f458225
Zhang Erning <zhang@erning.com>
2021-10-20 13:17:11 +0800

    remove rust

--- build.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-use std::fs;
-use std::path::Path;
-
-fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("modules.rs");
-    fs::write(&dest_path, "").unwrap();
-}
