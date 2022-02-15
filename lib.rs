0e80781e13b3910ecf62e4ee1ea58e1943c05f6b
Zhang Erning <zhang@erning.com>
2022-02-15 17:34:19 +0800

    0003 in rust

--- lib.rs
+++ lib.rs
@@ -4,6 +4,9 @@ pub mod problem_0001;
 #[path = "0002-add-two-numbers/add_two_numbers.rs"]
 pub mod problem_0002;
 
+#[path = "0003-longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.rs"]
+pub mod problem_0003;
+
 #[path = "0051-n-queens/n_queens.rs"]
 pub mod problem_0051;
 
