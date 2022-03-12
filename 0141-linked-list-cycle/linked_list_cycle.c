89a54203ac1c1e419fad460b85c25c33b7901a36
Zhang Erning <zhang@erning.com>
2022-03-12 21:57:22 +0800

    141 refactor

--- 0141-linked-list-cycle/linked_list_cycle.c
+++ 0141-linked-list-cycle/linked_list_cycle.c
@@ -8,11 +8,9 @@ struct ListNode {
 bool hasCycle(struct ListNode *head) {
     struct ListNode* p1 = head;
     struct ListNode* p2 = head;
-    while (p1 && p2) {
+    while (p2 && p2->next) {
         p1 = p1->next;
-        p2 = p2->next;
-        if (!p2) return false;
-        p2 = p2->next;
+        p2 = p2->next->next;
         if (p1 == p2) return true;
     }
     return false;
