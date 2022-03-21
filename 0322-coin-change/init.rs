3b4f5a3414280817b2c88938cc77e2bf4cb8cbfe
Zhang Erning <zhang@erning.com>
2022-03-21 14:20:19 +0800

    use MAX value instead of Option

--- 0322-coin-change/init.rs
+++ 0322-coin-change/init.rs
@@ -1,36 +1,25 @@
 pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-    let f = dp;
-    match f(&coins, amount) {
-        Some(count) => count,
-        None => -1,
-    }
-}
-
-fn dp(coins: &[i32], amount: i32) -> Option<i32> {
-    let mut dp: Vec<Option<i32>> = vec![None; amount as usize + 1];
-    dp[0].replace(0);
-
+    let mut dp: Vec<i32> = vec![amount + 1; amount as usize + 1];
+    dp[0] = 0;
     for i in 0..=amount {
         for coin in coins.iter() {
             let j = i - coin;
             if j < 0 {
                 continue;
             }
-            let i = i as usize;
-            let j = j as usize;
-            if let Some(prev) = dp[j] {
-                let next = prev + 1;
-                if let Some(count) = dp[i] {
-                    if count > next {
-                        dp[i].replace(next);
-                    }
-                } else {
-                    dp[i].replace(next);
-                }
+            let next = dp[j as usize] + 1;
+            let vi = &mut dp[i as usize];
+            if *vi > next {
+                *vi = next
             }
         }
     }
-    dp[amount as usize]
+    let count = dp[amount as usize];
+    if count > amount {
+        -1
+    } else {
+        count
+    }
 }
 
 #[allow(dead_code)]
