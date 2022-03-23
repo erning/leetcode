193d61f5491470c4621cc627f2685373af88512e
Zhang Erning <zhang@erning.com>
2022-03-23 21:19:57 +0800

    309 dp reduce

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/init.rs
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/init.rs
@@ -1,12 +1,19 @@
 pub fn max_profit(prices: Vec<i32>) -> i32 {
-    let len = prices.len();
-    let mut dp: Vec<Vec<i32>> = vec![vec![0; len + 1]; 2];
-    dp[0][1] = -prices[0];
-    for i in 1..len {
-        dp[0][i + 1] = i32::max(dp[0][i], dp[1][i - 1] - prices[i]);
-        dp[1][i + 1] = i32::max(dp[1][i], dp[0][i] + prices[i]);
-    }
-    i32::max(dp[0][len], dp[1][len])
+    let (a, _, b) = prices
+        .iter()
+        .fold((-prices[0], 0, 0), |(a2, b1, b2), price| {
+            (i32::max(a2, b1 - price), b2, i32::max(b2, a2 + price))
+        });
+    i32::max(a, b)
+
+    // let len = prices.len();
+    // let mut dp: Vec<Vec<i32>> = vec![vec![0; len + 1]; 2];
+    // dp[0][1] = -prices[0];
+    // for i in 1..len {
+    //     dp[0][i + 1] = i32::max(dp[0][i], dp[1][i - 1] - prices[i]);
+    //     dp[1][i + 1] = i32::max(dp[1][i], dp[0][i] + prices[i]);
+    // }
+    // i32::max(dp[0][len], dp[1][len])
 }
 
 #[cfg(test)]
