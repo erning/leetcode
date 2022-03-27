027a327b25f83f21f70a79a2805813f870188034
Zhang Erning <zhang@erning.com>
2022-03-27 21:21:38 +0800

    17 letter combinations of a phone number

---
+++ 0017-letter-combinations-of-a-phone-number/init.rs
@@ -0,0 +1,62 @@
+pub fn letter_combinations(digits: String) -> Vec<String> {
+    let map: [Vec<char>; 8] = [
+        vec!['a', 'b', 'c'],      // 2
+        vec!['d', 'e', 'f'],      // 3
+        vec!['g', 'h', 'i'],      // 4
+        vec!['j', 'k', 'l'],      // 5
+        vec!['m', 'n', 'o'],      // 6
+        vec!['p', 'q', 'r', 's'], // 7
+        vec!['t', 'u', 'v'],      // 8
+        vec!['w', 'x', 'y', 'z'], // 9
+    ];
+    let mut rv = Vec::<String>::with_capacity(4usize.pow(digits.len() as u32) as usize);
+    let mut iter = digits.as_bytes().iter();
+
+    if let Some(i) = iter.next() {
+        rv.append(
+            &mut map[(i - 0x32) as usize]
+                .iter()
+                .map(|c| c.to_string())
+                .collect(),
+        );
+    } else {
+        return rv;
+    }
+
+    while let Some(i) = iter.next() {
+        let len = rv.len();
+        let chars = &map[(i - 0x32) as usize];
+        for _ in 1..chars.len() {
+            rv.extend_from_within(..len);
+        }
+        let mut iter = rv.iter_mut();
+        for c in chars {
+            for _ in 0..len {
+                iter.next().unwrap().push(*c);
+            }
+        }
+    }
+
+    rv
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn tf(input: &str, expect: &[&str]) {
+        let mut output = letter_combinations(input.to_string());
+        output.sort();
+        assert_eq!(output, expect, "expect {:?} but {:?}", expect, output);
+    }
+
+    #[test]
+    fn example() {
+        tf(
+            "23",
+            &["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"],
+        );
+        tf("", &[]);
+        tf("2", &["a", "b", "c"]);
+    }
+}
--- lib.rs
+++ lib.rs
@@ -37,6 +37,9 @@ pub mod problem_0015;
 #[path="0016-3sum-closest/init.rs"]
 pub mod problem_0016;
 
+#[path="0017-letter-combinations-of-a-phone-number/init.rs"]
+pub mod problem_0017;
+
 #[path="0021-merge-two-sorted-lists/init.rs"]
 pub mod problem_0021;
 
