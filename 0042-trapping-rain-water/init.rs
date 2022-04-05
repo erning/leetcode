525d952af1c0d9f8261e5fd7c74830dc24abb7fa
Zhang Erning <zhang@erning.com>
2022-04-05 14:29:01 +0800

    42 - find the heighest, divide and caculate. finally O(n)

--- 0042-trapping-rain-water/init.rs
+++ 0042-trapping-rain-water/init.rs
@@ -1,37 +1,37 @@
 pub fn trap(height: Vec<i32>) -> i32 {
-    fn recursion(height: &[i32]) -> i32 {
-        if height.len() < 3 {
-            return 0;
+    fn area<'a, T>(mut iter: T) -> i32
+    where
+        T: Iterator<Item = &'a i32>,
+    {
+        let mut area = 0;
+        let mut prev = iter.next().unwrap();
+        while let Some(h) = iter.next() {
+            if h > prev {
+                prev = h;
+            } else {
+                area += prev - h;
+            }
         }
-        let a = (0, height[0]);
-        let b = (height.len() - 1, height[height.len() - 1]);
-        let c = height
-            .iter()
-            .enumerate()
-            .take(height.len() - 1)
-            .skip(1)
-            .map(|(i, &v)| (i, v))
-            .max_by(|x, y| x.1.cmp(&y.1))
-            .unwrap();
-
-        let area = |x: (usize, i32), y: (usize, i32)| -> i32 {
-            let h = i32::min(x.1, y.1);
-            let w = y.0 as i32 - x.0 as i32 - 1;
-            h * w - height[x.0 + 1..y.0].iter().sum::<i32>()
-        };
-
-        if c.1 <= a.1 && c.1 <= b.1 {
-            return area(a, b);
-        }
-        if c.1 <= a.1 {
-            return area(a, c) + recursion(&height[c.0..b.0 + 1]);
-        }
-        if c.1 <= b.1 {
-            return area(c, b) + recursion(&height[a.0..c.0 + 1]);
-        }
-        return recursion(&height[a.0..c.0 + 1]) + recursion(&height[c.0..b.0 + 1]);
+        area
     }
-    recursion(&height[..])
+
+    let len = height.len();
+    if len < 3 {
+        return 0;
+    }
+    let heightest = height
+        .iter()
+        .enumerate()
+        .max_by(|x, y| x.1.cmp(&y.1))
+        .unwrap();
+
+    if heightest.0 == 0 {
+        area(height.iter().rev())
+    } else if heightest.0 == len {
+        area(height.iter())
+    } else {
+        area(height[..heightest.0 + 1].iter()) + area(height[heightest.0..].iter().rev())
+    }
 }
 
 #[cfg(test)]
