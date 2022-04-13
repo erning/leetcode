912eb37ac66b3380b41b04757fd540a37bc4a55e
Zhang Erning <zhang@erning.com>
2022-04-13 21:55:19 +0800

    93 - restore ip addresses. as_str() instead of as_bytes()

--- 0093-restore-ip-addresses/init.rs
+++ 0093-restore-ip-addresses/init.rs
@@ -1,23 +1,19 @@
 pub fn restore_ip_addresses(s: String) -> Vec<String> {
     #[inline]
-    fn is_valid_byte(s: &[u8]) -> bool {
-        if s.len() > 1 && s[0] == b'0' {
+    fn is_valid_byte(s: &str) -> bool {
+        if s.len() > 1 && &s[0..1] == "0" {
             return false;
         }
-        let mut v = 0;
-        for &c in s.iter() {
-            v = v * 10 + (c - b'0') as u32;
-        }
-        v <= 255
+        return s.parse::<u8>().is_ok();
     }
 
-    let s = s.as_bytes();
+    let s = s.as_str();
     let len = s.len();
     if len < 4 || len > 12 {
         return vec![];
     }
-    let mut rv = Vec::new();
 
+    let mut rv = Vec::new();
     for a in 1..=usize::min(3, len - 3) {
         let s1 = &s[..a];
         if !is_valid_byte(s1) {
@@ -29,30 +25,19 @@ pub fn restore_ip_addresses(s: String) -> Vec<String> {
                 continue;
             }
             for c in b + 1..=usize::min(b + 3, len - 1) {
-                let s3 = &s[b..c];
-                if !is_valid_byte(s3) {
-                    continue;
-                }
                 if len - c > 3 {
                     continue;
                 }
+                let s3 = &s[b..c];
                 let s4 = &s[c..];
-                if !is_valid_byte(s4) {
+                if !is_valid_byte(s3) || !is_valid_byte(s4) {
                     continue;
                 }
-
-                let v = format!(
-                    "{}.{}.{}.{}",
-                    String::from_utf8(s1.to_vec()).unwrap(),
-                    String::from_utf8(s2.to_vec()).unwrap(),
-                    String::from_utf8(s3.to_vec()).unwrap(),
-                    String::from_utf8(s4.to_vec()).unwrap()
-                );
+                let v = format!("{}.{}.{}.{}", s1, s2, s3, s4);
                 rv.push(v);
             }
         }
     }
-
     rv
 }
 
