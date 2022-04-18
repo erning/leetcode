72fdab879ffa876db8c2ca139f81aec6f9908b9a
Zhang Erning <zhang@erning.com>
2022-04-18 16:56:53 +0800

    128 - refactor hashset only

--- 0128-longest-consecutive-sequence/init.rs
+++ 0128-longest-consecutive-sequence/init.rs
@@ -1,27 +1,20 @@
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-    let nums: HashSet<i32> = nums.into_iter().collect();
-    let mut visited: HashSet<i32> = HashSet::new();
-    let mut cache: HashMap<i32, i32> = HashMap::new();
-
+    let mut remains: HashSet<i32> = nums.iter().map(|v| *v).collect();
     let mut longest = 0;
-    for &n in nums.iter() {
-        if visited.contains(&n) {
-            continue;
-        }
+    while let Some(&n) = remains.iter().next() {
+        remains.remove(&n);
         let mut m = n + 1;
-        while nums.contains(&m) {
-            if let Some(k) = cache.get(&m) {
-                m += k;
-                break;
-            }
-            visited.insert(m);
+        while let Some(_) = remains.take(&m) {
             m += 1;
         }
-        let count = m - n;
-        cache.insert(n, count);
+        let mut count = m - n;
+        m = n - 1;
+        while let Some(_) = remains.take(&m) {
+            m -= 1;
+        }
+        count += n - m - 1;
         if count > longest {
             longest = count;
         }
@@ -35,6 +28,7 @@ mod tests {
 
     #[test]
     fn example() {
+        assert_eq!(longest_consecutive(vec![4, 3, 2, 1]), 4);
         assert_eq!(longest_consecutive(vec![100, 4, 200, 1, 3, 2]), 4);
         assert_eq!(longest_consecutive(vec![0, 3, 7, 2, 5, 8, 4, 6, 0, 1]), 9);
     }
