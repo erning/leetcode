bb0ac3c24e2eaf61c8f2d229a3d7da27156c85b7
Zhang Erning <zhang@erning.com>
2022-04-22 12:36:19 +0800

    152 - maximum product subarray. O(N)

--- 0152-maximum-product-subarray/init.rs
+++ 0152-maximum-product-subarray/init.rs
@@ -1,22 +1,15 @@
 pub fn max_product(nums: Vec<i32>) -> i32 {
-    let n = nums.len();
-    let mut max = i32::MIN;
-    let mut prev: Vec<i32> = Vec::with_capacity(n);
-    for &v in nums.iter() {
-        if v > max {
-            max = v;
+    let mut max = nums[0];
+    let mut curr_min = max;
+    let mut curr_max = max;
+    for v in nums.into_iter().skip(1) {
+        let a = curr_min * v;
+        let b = curr_max * v;
+        curr_min = v.min(a).min(b);
+        curr_max = v.max(a).max(b);
+        if curr_max > max {
+            max = curr_max;
         }
-        prev.push(v);
-    }
-    let mut curr: Vec<i32> = vec![0; n];
-    for i in 1..n {
-        for (j, v) in curr.iter_mut().enumerate().skip(i) {
-            *v = prev[j - 1] * nums[j];
-            if *v > max {
-                max = *v;
-            }
-        }
-        std::mem::swap(&mut curr, &mut prev);
     }
     max
 }
