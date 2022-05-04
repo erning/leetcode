42d4644aa69224e7a251b69a53da0a9fe5c5d722
Zhang Erning <zhang@erning.com>
2022-05-04 13:15:19 +0800

    1679 - max number of k sum pairs. sort and two pointers

--- 1679-max-number-of-k-sum-pairs/init.rs
+++ 1679-max-number-of-k-sum-pairs/init.rs
@@ -1,34 +1,20 @@
-use std::collections::HashMap;
-
 pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
-    let mut map: HashMap<i32, i32> = HashMap::new();
-    for &v in nums.iter().filter(|&&v| v < k) {
-        if let Some(m) = map.get_mut(&v) {
-            *m += 1;
-        } else {
-            map.insert(v, 1);
-        }
-    }
+    let mut nums = nums;
+    nums.sort_unstable();
     let mut count = 0;
-    for a in nums.into_iter().filter(|&v| v < k) {
-        if let Some(c) = map.get_mut(&a) {
-            *c -= 1;
-            if *c <= 0 {
-                map.remove(&a);
-            }
+    let mut i = 0;
+    let mut j = nums.len() - 1;
+    while i < j {
+        let c = nums[i] + nums[j];
+        if c > k {
+            j -= 1;
+        } else if c < k {
+            i += 1;
         } else {
-            continue;
+            count += 1;
+            i += 1;
+            j -= 1;
         }
-        let b = k - a;
-        if let Some(c) = map.get_mut(&b) {
-            *c -= 1;
-            if *c <= 0 {
-                map.remove(&b);
-            }
-        } else {
-            continue;
-        }
-        count += 1;
     }
     count
 }
