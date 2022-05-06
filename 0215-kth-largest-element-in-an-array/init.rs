6fd19c3fc4022e581f933ac617290406b5da9b9b
Zhang Erning <zhang@erning.com>
2022-05-06 14:16:01 +0800

    215 - kth largest element in an array. quick-select with middle as pivot

--- 0215-kth-largest-element-in-an-array/init.rs
+++ 0215-kth-largest-element-in-an-array/init.rs
@@ -1,7 +1,36 @@
+pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+    let k = k as usize - 1;
+    let n = nums.len();
+    let mut nums = nums;
+    let mut lo = 0;
+    let mut hi = n - 1;
+    while lo < hi {
+        let m = lo + (hi - lo) / 2;
+        nums.swap(m, hi);
+        let pivot = nums[hi];
+        let mut p = lo;
+        for i in lo..hi {
+            if nums[i] > pivot {
+                nums.swap(p, i);
+                p += 1;
+            }
+        }
+        nums.swap(p, hi);
+        if p < k {
+            lo = p + 1;
+        } else if p > k {
+            hi = p - 1;
+        } else {
+            break;
+        }
+    }
+    nums[k]
+}
+
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 
-pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+pub fn find_kth_largest_heap(nums: Vec<i32>, k: i32) -> i32 {
     let k = k as usize;
     let mut heap: BinaryHeap<Reverse<i32>> = BinaryHeap::with_capacity(k + 1);
     for n in nums.into_iter() {
@@ -25,5 +54,7 @@ mod tests {
     fn example() {
         assert_eq!(find_kth_largest(vec![3, 2, 1, 5, 6, 4], 2), 5);
         assert_eq!(find_kth_largest(vec![3, 2, 3, 1, 2, 4, 5, 5, 6], 4), 4);
+
+        assert_eq!(find_kth_largest(vec![7, 6, 5, 4, 3, 2, 1], 2), 6);
     }
 }
