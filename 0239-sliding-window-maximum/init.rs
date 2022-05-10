a78ed17cbef82e91aa1ed6338ce94dcb78d586b4
Zhang Erning <zhang@erning.com>
2022-05-10 14:13:15 +0800

    239 - sliding window maximum. monotonic double ended queue

--- 0239-sliding-window-maximum/init.rs
+++ 0239-sliding-window-maximum/init.rs
@@ -1,40 +1,28 @@
-use std::collections::BinaryHeap;
+use std::collections::VecDeque;
 
 pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let k = k as usize;
     let mut answer = Vec::with_capacity(k);
-    const MAX: i32 = 10000;
-    let mut heap: BinaryHeap<i32> = BinaryHeap::new();
-    let mut map: [i32; MAX as usize * 2 + 1] = [0; MAX as usize * 2 + 1];
-    let mut max = i32::MIN;
-    for &v in nums.iter().take(k) {
-        if v > max {
-            max = v;
+    let mut mq = VecDeque::new(); // monotonic queue
+    for v in nums.iter().take(k - 1) {
+        while !mq.is_empty() && mq.back().unwrap() < v {
+            mq.pop_back();
         }
-        heap.push(v);
-        map[(v + MAX) as usize] += 1;
+        mq.push_back(*v);
     }
-    answer.push(max);
-    for i in k + 1..=nums.len() {
-        let removed = nums[i - k - 1];
-        map[(removed + MAX) as usize] -= 1;
-        let added = nums[i - 1];
-        map[(added + MAX) as usize] += 1;
-        heap.push(added);
-
-        if added > max {
-            max = added;
-        } else if removed == max && added < max {
-            loop {
-                max = *heap.peek().unwrap();
-                if map[(max + MAX) as usize] > 0 {
-                    break;
-                }
-                heap.pop();
-            }
+    for i in k - 1..nums.len() {
+        let added = nums[i];
+        while !mq.is_empty() && mq.back().unwrap() < &added {
+            mq.pop_back();
         }
-
+        mq.push_back(added);
+        let max = *mq.front().unwrap();
         answer.push(max);
+
+        let removed = nums[i + 1 - k];
+        if removed == max {
+            mq.pop_front();
+        }
     }
     answer
 }
