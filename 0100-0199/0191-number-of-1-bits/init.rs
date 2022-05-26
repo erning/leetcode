4be2445791b07eb14fd75f3f62d65bb69f1166a1
Zhang Erning <zhang@erning.com>
2022-05-26 11:00:29 +0800

    191 - number of 1 bits.

--- 0100-0199/0191-number-of-1-bits/init.rs
+++ 0100-0199/0191-number-of-1-bits/init.rs
@@ -3,12 +3,10 @@ pub fn hammingWeight(n: u32) -> i32 {
     let mut n = n;
     let mut count = 0;
     while n > 0 {
-        if n & 1 > 0 {
-            count += 1;
-        }
+        count += n & 1;
         n >>= 1;
     }
-    count
+    count as i32
 }
 
 #[cfg(test)]
