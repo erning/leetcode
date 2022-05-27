04afdbe8dc21dc23a7416a43e9ebbfa4a2f38573
Zhang Erning <zhang@erning.com>
2022-05-27 10:16:15 +0800

    1342 - number of -steps to reduce a number to zero. count step by bits

--- 1342-number-of-steps-to-reduce-a-number-to-zero/init.rs
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/init.rs
@@ -1,13 +1,9 @@
 pub fn number_of_steps(num: i32) -> i32 {
-    let mut step = 0;
-    let mut n = num;
+    let mut step = num & 0x01;
+    let mut n = num >> 1;
     while n > 0 {
-        step += 1;
-        if n & 0x01 == 0 {
-            n >>= 1;
-        } else {
-            n -= 1;
-        }
+        step += (n & 0x01) + 1;
+        n >>= 1;
     }
     step
 }
@@ -21,5 +17,7 @@ mod tests {
         assert_eq!(number_of_steps(14), 6);
         assert_eq!(number_of_steps(8), 4);
         assert_eq!(number_of_steps(123), 12);
+
+        assert_eq!(number_of_steps(0), 0);
     }
 }
