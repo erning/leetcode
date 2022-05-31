96125793e78b9b3d0e2546d17f0bdf668407d848
Zhang Erning <zhang@erning.com>
2022-05-31 19:26:07 +0800

    69 - sqrtx. binary search

--- 0000-0099/0069-sqrtx/init.rs
+++ 0000-0099/0069-sqrtx/init.rs
@@ -1,65 +1,20 @@
 use std::cmp::Ordering;
 
 pub fn my_sqrt(x: i32) -> i32 {
-    let mut n = x;
-    let mut stack = Vec::with_capacity(5);
-    while n >= 100 {
-        let m = n % 100;
-        n /= 100;
-        stack.push(m);
-    }
-    let mut y = if n == 0 {
-        0
-    } else if n < 2 * 2 {
-        1
-    } else if n < 3 * 3 {
-        2
-    } else if n < 4 * 4 {
-        3
-    } else if n < 5 * 5 {
-        4
-    } else if n < 6 * 6 {
-        5
-    } else if n < 7 * 7 {
-        6
-    } else if n < 8 * 8 {
-        7
-    } else if n < 9 * 9 {
-        8
-    } else if n < 10 * 10 {
-        9
-    } else {
-        unimplemented!()
-    };
-    while let Some(m) = stack.pop() {
-        n = n * 100 + m;
-        y *= 10;
-        for _ in 0..10 {
-            if y >= 46340 {
-                // max sqrt for i32 to avoid overflow
-                break;
+    let mut a = 1;
+    let mut b = x;
+    while a <= b {
+        let c = a + (b - a) / 2;
+        println!("{:?}", (a, b, c));
+        match (x / c).cmp(&c) {
+            Ordering::Less => b = c - 1,
+            Ordering::Greater => a = c + 1,
+            Ordering::Equal => {
+                return c;
             }
-            y += 1;
-            let z = y * y;
-            match z.cmp(&n) {
-                Ordering::Greater => {
-                    y -= 1;
-                    break;
-                }
-                Ordering::Equal => {
-                    break;
-                }
-                _ => {}
-            }
-            // if z > n {
-            //     y -= 1;
-            //     break;
-            // } else if z == n {
-            //     break;
-            // }
         }
     }
-    y
+    a - 1
 }
 
 #[cfg(test)]
