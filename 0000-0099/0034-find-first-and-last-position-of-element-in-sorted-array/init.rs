873245287ac84088b44cfb8d2981d3822aef4f8e
Zhang Erning <zhang@erning.com>
2022-06-01 13:13:13 +0800

    34 - find first and last position of element in sorted array. improved

--- 0000-0099/0034-find-first-and-last-position-of-element-in-sorted-array/init.rs
+++ 0000-0099/0034-find-first-and-last-position-of-element-in-sorted-array/init.rs
@@ -1,60 +1,36 @@
-use std::cmp::Ordering;
-
 pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    fn f(nums: &[i32], target: i32, a: usize, b: usize) -> Option<usize> {
-        let c = a + (b - a) / 2;
-        if c == a {
-            return if nums[a] == target {
-                Some(a)
-            } else if nums[b] == target {
-                Some(b)
-            } else {
-                None
-            };
-        }
-        match target.cmp(&nums[c]) {
-            Ordering::Less => f(nums, target, a, c - 1),
-            Ordering::Greater => f(nums, target, c + 1, b),
-            _ => Some(c),
+    // a = index of first target
+    let mut c = match nums.binary_search(&target) {
+        Err(_) => return vec![-1, -1],
+        Ok(i) => i,
+    };
+    let mut a = 0;
+    while a != c {
+        match &nums[..c].binary_search(&target) {
+            Err(i) => {
+                a = *i;
+                break;
+            }
+            Ok(i) => c = *i,
         }
     }
-
-    fn p1(nums: &[i32], target: i32, a: usize, b: usize) -> usize {
-        if nums[a] == target {
-            return a;
-        }
-        let c = a + (b - a) / 2;
-        if nums[c] == target {
-            p1(nums, target, a + 1, c)
-        } else {
-            p1(nums, target, c + 1, b)
-        }
-    }
-
-    fn p2(nums: &[i32], target: i32, a: usize, b: usize) -> usize {
-        if nums[b] == target {
-            return b;
-        }
-        let c = a + (b - a) / 2;
-        if nums[c] == target {
-            p2(nums, target, c, b - 1)
-        } else {
-            p2(nums, target, a, c - 1)
+    // b = index of first target+1
+    let mut c = match &nums[a + 1..].binary_search(&(target + 1)) {
+        Err(i) => return vec![a as i32, (a + i) as i32],
+        Ok(i) => a + 1 + i,
+    };
+    let mut b = a + 1;
+    while b != c {
+        match &nums[a + 1..c].binary_search(&(target + 1)) {
+            Err(i) => {
+                b = a + 1 + *i;
+                break;
+            }
+            Ok(i) => c = a + 1 + *i,
         }
     }
-
-    if nums.is_empty() {
-        return vec![-1, -1];
-    }
-    match f(&nums, target, 0, nums.len() - 1) {
-        Some(p) => {
-            vec![
-                p1(&nums, target, 0, p) as i32,
-                p2(&nums, target, p, nums.len() - 1) as i32,
-            ]
-        }
-        None => vec![-1, -1],
-    }
+    // [a, b-1]
+    vec![a as i32, b as i32 - 1]
 }
 
 #[cfg(test)]
